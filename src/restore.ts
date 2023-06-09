4c926e8f492d1d2fd0f8654095b489efedf4ce0c
Asher Hoeveler <theargon0@gmail.com>
2023-06-09 01:43:56 -0500

    rework restorer

--- src/restore.ts
+++ src/restore.ts
@@ -1,6 +1,19 @@
 import fs from 'fs';
 import discord from 'discord.js';
 
+type categorymeta = {
+    type: 4,
+    guild: string,
+    guildid: string,
+    permissionOverrides: Array<any>,
+    flags: number,
+    id: string,
+    name: string,
+    rawPosition: number,
+    parentId: null|string,
+    createdTimestamp: number
+}
+
 const { token, guildid } = JSON.parse(fs.readFileSync('../.creds.json').toString()).restorer
 
 function restoreServer() {
@@ -25,51 +38,10 @@ function restoreServer() {
                 })
             });
         });
-        let categories: Map<string, discord.CategoryChannel> = new Map<string, discord.CategoryChannel>();
-        let hooks: Map<discord.GuildTextBasedChannel, discord.Webhook> = new Map<discord.GuildTextBasedChannel, discord.Webhook>();
-        let channels: Map<string, discord.GuildTextBasedChannel> = new Map<string, discord.GuildTextBasedChannel>();
-
-        for (let folderName of fs.readdirSync('../serverclone/channels')) {
-            console.log(`Creating channel category ${folderName}`);
-            let category = await ClonePod.channels.create({
-                name: folderName,
-                type: discord.ChannelType.GuildCategory,
-                // topic: JSON.parse(fs.readFileSync(`../serverclone/channels/${folder}/metadata`).toString())
-            })
-            categories.set(`../serverclone/channels/${folderName}`, category);
+        for (let folder of fs.readdirSync('../serverclone/channels')) {
+            let ccat:categorymeta = JSON.parse(fs.readFileSync(`../serverclone/channels/${folder}`).toString());
+            //TODO the rest of this bc downloading a ton of stuff is lagging my IDE
         }
-        for (let [folderPath, channel] of categories) {
-            for (let subItem of fs.readdirSync(folderPath)) {
-                if (!fs.lstatSync(`${folderPath}/${subItem}`).isDirectory()) {
-                    console.log(`${folderPath}/${subItem} is not a channel folder`);
-                    continue;
-                }
-                console.log(`Creating channel #${subItem}`)
-                let textChannel = await ClonePod.channels.create({
-                    name: subItem,
-                    type: discord.ChannelType.GuildText,
-                    parent: channel
-                })
-                console.log(`Creating webhook in ${subItem}`);
-                let hook = await textChannel.createWebhook({ name: 'Mirror world' });
-                hooks.set(textChannel, hook);
-                channels.set(`${folderPath}/${subItem}`, textChannel);
-            }
-        }
-
-        //HACK REMOVE THIS
-        console.log('CLEANING UP ATTEMPT...');
-        for (let [_, hook] of hooks) {
-            hook.delete('Cleanup');
-        }
-        for (let [_, channel] of channels) {
-            await channel.delete('Cleanup');
-        }
-        for (let [_, channel] of categories) {
-            await channel.delete('Cleanup');
-        }
-        bot.destroy();
-        console.log('Done restoring!');
     });
 
     bot.rest.on('rateLimited', (info) => {
