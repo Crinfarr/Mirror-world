8b774f64334a4853465cfbf5494dbf6060d71686
Asher Hoeveler <theargon0@gmail.com>
2023-06-09 11:04:48 -0500

    typedefs

--- src/restore.ts
+++ src/restore.ts
@@ -1,17 +1,41 @@
 import fs from 'fs';
 import discord from 'discord.js';
 
-type categorymeta = {
-    type: 4,
-    guild: string,
-    guildid: string,
-    permissionOverrides: Array<any>,
-    flags: number,
-    id: string,
-    name: string,
-    rawPosition: number,
+type categorymeta = channelmeta & {
+    type: discord.ChannelType.GuildCategory, //4
+}
+type textmeta = channelmeta & {
+    type: discord.ChannelType.GuildText, //0
+    messages: Array<string>,
+    threads: Array<string>,
+    nsfw: boolean,
+    topic: string,
+    lastMessageId: string,
+    lastPinTimestamp: number,
+    rateLimitPerUser: number
+}
+type voicemeta = channelmeta & {
+    type: discord.ChannelType.GuildVoice, //2
+    messages:Array<string>,
+    nsfw: boolean,
+    rtcregion:null|string,
+    bitrate:number,
+    userlimit:number,
+    videoQualityMode:null|string,
+    lastMessageId:null|string,
+    rateLimitPerUser:number
+}
+type channelmeta = {
+    type:number|discord.ChannelType,
+    guild:string,
+    guildid:string,
+    permissionOverwrites:Array<string>,
+    flags:number,
+    id:string,
+    name:string,
+    rawPosition:number,
     parentId: null|string,
-    createdTimestamp: number
+    createdTimestamp:number
 }
 
 const { token, guildid } = JSON.parse(fs.readFileSync('../.creds.json').toString()).restorer
@@ -39,8 +63,15 @@ function restoreServer() {
             });
         });
         for (let folder of fs.readdirSync('../serverclone/channels')) {
-            let ccat:categorymeta = JSON.parse(fs.readFileSync(`../serverclone/channels/${folder}`).toString());
-            //TODO the rest of this bc downloading a ton of stuff is lagging my IDE
+            let ccat:categorymeta = JSON.parse(fs.readFileSync(`../serverclone/channels/${folder}/metadata`).toString());
+            let category = await ClonePod.channels.create(ccat);
+            for (let subfolder of fs.readdirSync(`../serverclone/channels/${folder}`)) {
+                if (subfolder == 'metadata') {
+                    //ignore metadata
+                    continue;
+                }
+                let cchannel:
+            }
         }
     });
 
