5892fbc14695a26dd0a9c79f2a7362f6022ed03b
Asher Hoeveler <theargon0@gmail.com>
2023-06-25 02:09:54 -0500

    nightly

--- src/backup.ts
+++ src/backup.ts
@@ -5,7 +5,6 @@ import fs from 'fs';
 import https from 'https';
 
 export function backupServer(serverid: string) {
-    const { token } = JSON.parse(fs.readFileSync('../.creds.json').toString());
     return new Promise<Client<true>>(async (backupComplete, backupErrored) => {
         const bot = new Client({
             intents: [
@@ -73,19 +72,19 @@ export function backupServer(serverid: string) {
                     );
                     `);
                     if (channel.type == ChannelType.GuildCategory) {
-                        process.stdout.write('📂');
+                        process.stdout.write(`📁Creating category ${channel.name}                                        \n`);
                     }
                     else if (channel.type == ChannelType.GuildText) {
-                        process.stdout.write('❔');
+                        process.stdout.write(`❔Trying fetch operation on ${channel.name}                                        \n`);
                         try {
                             await channel.messages.fetch({
                                 limit: 1
                             });
-                            process.stdout.write('✔');
+                            process.stdout.write(`✔Fetch succeeded on ${channel.name}                                        \n`);
                         }
                         catch (error) {
                             console.error(error);
-                            process.stdout.write('❌');
+                            process.stdout.write(`❌Fetch failed on ${channel.name}; Not backing up                                        \n`);
                             continue;
                         }
 
@@ -106,35 +105,44 @@ export function backupServer(serverid: string) {
                                 let attachments = [];
                                 for (let [_id, attachment] of msg.attachments) {
                                     const afile = fs.createWriteStream('tmp.bin');
-                                    process.stdout.write('🗋');
+                                    let downloadFailed = false;
+                                    process.stdout.write(`🗋Downloading file ${attachment.name}                                       \n`);
                                     await new Promise((res, rej) => {
-                                        https.get(attachment.url, (response) => {
-                                            response.pipe(afile);
+                                        https.get(attachment.proxyURL, (response) => {
+                                            response.on('error', (e) => {
+                                                process.stdout.write(`💥Download failed for ${attachment.name}`);
+                                                downloadFailed = true;
+                                                //FwCOooAWYAEH9c9.jpg makes this break
+                                                res(null);
+                                            })
                                             afile.on('finish', () => {
                                                 afile.close();
                                                 res(null);
                                             });
                                             afile.on('error', () => {
                                                 rej(null);
-                                            })
+                                            });
+                                            response.pipe(afile);
                                         })
                                     });
-                                    process.stdout.write('✔');
+                                    if (downloadFailed) continue;
+                                    process.stdout.write(`✔File ${attachment.name} downloaded                                        \n`);
                                     attachments.push(
                                         {
                                             type: 'attachment',
+                                            filename: attachment.name,
                                             content: fs.readFileSync('tmp.bin').toString('base64')
                                         }
                                     );
                                     fs.rmSync('tmp.bin');
-                                    process.stdout.write('🔒');
+                                    process.stdout.write(`🔒File ${attachment.name} saved                                        \n`);
                                 }
                                 for (let [_id, sticker] of msg.stickers) {
                                     const afile = fs.createWriteStream('tmp.bin');
-                                    process.stdout.write('🗖');
+                                    process.stdout.write(`🗖Downloading sticker ${sticker.name}                                        \n`);
                                     await new Promise((res, rej) => {
                                         https.get(sticker.url, (response) => {
-                                            response.pipe(afile);
+
                                             response.on('error', () => {
                                                 res(null);
                                             })
@@ -144,23 +152,25 @@ export function backupServer(serverid: string) {
                                             });
                                             afile.on('error', () => {
                                                 rej(null);
-                                            })
+                                            });
+                                            response.pipe(afile);
                                         })
                                     });
-                                    process.stdout.write('✔');
+                                    process.stdout.write(`✔Sticker ${sticker.name} downloaded                                        \n`);
                                     attachments.push(
                                         {
                                             type: 'sticker',
+                                            filename: /(?<=stickers\/).+\..+$/gm.exec(sticker.url),
                                             content: fs.readFileSync('tmp.bin').toString('base64')
                                         }
                                     );
                                     fs.rmSync('tmp.bin');
-                                    process.stdout.write('🔒');
+                                    process.stdout.write(`🔒Sticker ${sticker.name} saved                                        \n`);
                                 }
                                 for (let embed of msg.embeds) {
-                                    process.stdout.write('🗔');
+                                    process.stdout.write(`🗔Downloading embed                                        \n`);
                                     attachments.push({
-                                        type: embed,
+                                        type: 'embed',
                                         content: embed
                                     });
                                     process.stdout.write('🔒');
@@ -172,17 +182,18 @@ export function backupServer(serverid: string) {
                                         const afile = fs.createWriteStream('tmp.bin');
                                         let NOAVATAR = false;
                                         await new Promise((resolve2, reject2) => {
-                                            process.stdout.write('👤');
-                                            if (msg.author.avatarURL({ forceStatic: true }) == 'null') {
+                                            process.stdout.write(`👤Downloading user ${msg.author.username} avatar                                        \n`);
+                                            if (typeof msg.author.avatarURL({ forceStatic: true }) !== 'string' || msg.author.avatarURL({ forceStatic: true }) == 'null') {
                                                 afile.close();
-                                                process.stdout.write('✖');
+                                                process.stdout.write(`✖User ${msg.author.username} has no avatar                                        \n`);
                                                 NOAVATAR = true;
                                                 resolve2(null);
+                                                return;
                                             }
                                             https.get(msg.author.avatarURL({ forceStatic: true })!, (response) => {
-                                                response.pipe(afile);
-                                                response.on('error', () => {
-                                                    process.stdout.write('💥');
+
+                                                response.on('error', (e) => {
+                                                    process.stdout.write(`💥Error downloading avatar from user ${msg.author.username}                                   \n`);
                                                     NOAVATAR = true;
                                                     resolve2(null);
                                                 });
@@ -194,9 +205,10 @@ export function backupServer(serverid: string) {
                                                 afile.on('error', () => {
                                                     reject2(null);
                                                 });
+                                                response.pipe(afile);
                                             });
                                         })
-                                        process.stdout.write('🔒');
+                                        process.stdout.write('🔒Avatar downloaded successfully');
                                         await db.exec(`
                                         INSERT INTO Users (
                                             uid,
@@ -234,10 +246,10 @@ export function backupServer(serverid: string) {
                                     ${msg.editedTimestamp}
                                 )
                                 `);
-                                process.stdout.write('🗪');
+                                process.stdout.write(`🗪Message ${msg.id} logged                                        \n`);
                             }
                             if (messages.size < 100) {
-                                process.stdout.write('🗍');
+                                process.stdout.write(`🗍All messages backed up from ${channel.name}                                        \n\n`);
                                 break;
                             }
                         }
@@ -246,6 +258,144 @@ export function backupServer(serverid: string) {
                 backupComplete(client);
             })
         })
+        bot.on('messageCreate', async (msg) => {
+            let attachments = [];
+            for (let [_id, attachment] of msg.attachments) {
+                const afile = fs.createWriteStream('tmp.bin');
+                process.stdout.write(`🗋Downloading file ${attachment.name}                                       \n`);
+                await new Promise((res, rej) => {
+                    https.get(attachment.proxyURL, (response) => {
+                        afile.on('finish', () => {
+                            afile.close();
+                            res(null);
+                        });
+                        afile.on('error', () => {
+                            rej(null);
+                        })
+                        response.pipe(afile);
+                    });
+                });
+                process.stdout.write(`✔File ${attachment.name} downloaded                                        \n`);
+                attachments.push(
+                    {
+                        type: 'attachment',
+                        filename: attachment.name,
+                        content: fs.readFileSync('tmp.bin').toString('base64')
+                    }
+                );
+                fs.rmSync('tmp.bin');
+                process.stdout.write(`🔒File ${attachment.name} saved                                        \n`);
+            }
+            for (let [_id, sticker] of msg.stickers) {
+                const afile = fs.createWriteStream('tmp.bin');
+                process.stdout.write(`🗖Downloading sticker ${sticker.name}                                        \n`);
+                await new Promise((res, rej) => {
+                    https.get(sticker.url, (response) => {
+                        response.on('error', () => {
+                            res(null);
+                        });
+                        afile.on('finish', () => {
+                            afile.close();
+                            res(null);
+                        });
+                        afile.on('error', () => {
+                            rej(null);
+                        });
+                        response.pipe(afile);
+                    })
+                });
+                process.stdout.write(`✔Sticker ${sticker.name} downloaded                                        \n`);
+                attachments.push(
+                    {
+                        type: 'sticker',
+                        filename: /(?<=stickers\/).+\..+$/gm.exec(sticker.url),
+                        content: fs.readFileSync('tmp.bin').toString('base64')
+                    }
+                );
+                fs.rmSync('tmp.bin');
+                process.stdout.write(`🔒Sticker ${sticker.name} saved                                        \n`);
+            }
+            for (let embed of msg.embeds) {
+                process.stdout.write(`🗔Downloading embed                                        \n`);
+                attachments.push({
+                    type: embed,
+                    content: embed
+                });
+                process.stdout.write('🔒');
+            }
+            await new Promise(async (resolve, _reject) => {
+                // process.stdout.write('❔');
+                //insert user into db if they don't exist in it already
+                if (!await db.get(`SELECT * FROM Users WHERE uid IS "${msg.author.id}";`)) {
+                    const afile = fs.createWriteStream('tmp.bin');
+                    let NOAVATAR = false;
+                    await new Promise((resolve2, reject2) => {
+                        process.stdout.write(`👤Downloading user ${msg.author.username} avatar                                        \n`);
+                        if (typeof msg.author.avatarURL({ forceStatic: true }) !== 'string' || msg.author.avatarURL({ forceStatic: true }) == 'null') {
+                            afile.close();
+                            process.stdout.write(`✖User ${msg.author.username} has no avatar                                        \n`);
+                            NOAVATAR = true;
+                            resolve2(null);
+                            return;
+                        }
+                        https.get(msg.author.avatarURL({ forceStatic: true })!, (response) => {
+
+                            response.on('error', (e) => {
+                                process.stdout.write(`💥Error downloading avatar from user ${msg.author.username}                                   \n`);
+                                NOAVATAR = true;
+                                resolve2(null);
+                            });
+                            afile.on('finish', () => {
+                                afile.close();
+                                process.stdout.write('✔');
+                                resolve2(null);
+                            });
+                            afile.on('error', () => {
+                                reject2(null);
+                            });
+                            response.pipe(afile);
+                        });
+                    })
+                    process.stdout.write('🔒Avatar downloaded successfully');
+                    await db.exec(`
+                    INSERT INTO Users (
+                        uid,
+                        avatar,
+                        username
+                    ) VALUES (
+                        "${msg.author.id}",
+                        "${(NOAVATAR) ? null : fs.readFileSync('tmp.bin').toString('base64')}",
+                        "${msg.author.username}"
+                    )
+                    `);
+                    fs.rmSync('tmp.bin');
+                    resolve(null);
+                }
+                else {
+                    resolve(null);
+                }
+            });
+            await db.exec(`
+            INSERT INTO Messages (
+                id,
+                channelid,
+                authorid,
+                content,
+                attachments,
+                created,
+                edited
+            ) VALUES (
+                "${msg.id}",
+                "${msg.channelId}",
+                "${msg.author.id}",
+                "${Buffer.from(msg.content).toString('base64')}",
+                "${Buffer.from(JSON.stringify(attachments)).toString('base64')}",
+                ${msg.createdTimestamp},
+                ${msg.editedTimestamp}
+            )
+            `);
+            process.stdout.write(`New message ${msg.id} logged                                   \n`)
+        });
         bot.login(JSON.parse(fs.readFileSync('../.creds.json').toString()).archiver.token);
     });
 }
